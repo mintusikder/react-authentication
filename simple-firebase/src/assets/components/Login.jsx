981f1eec1f532972be9b1fa507405645d6f57de8
mintusikder <mintusikder15@gmail.com>
2024-03-22 12:20:33 +0600

    signinwithpopup

--- simple-firebase/src/assets/components/Login.jsx
+++ simple-firebase/src/assets/components/Login.jsx
@@ -1,15 +1,32 @@
 // import React from 'react';
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../firebase/firebase.init";
+import { useState } from "react";
+
 const Login = () => {
+  const [user, setUser] = useState(null);
+  
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const handelGoogleLogin = () =>{
-    console.log("Button")
-  }
+  
+  const handelGoToGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
   return (
     <div>
-      <button onClick={handelGoogleLogin}>Google Login</button>
+      <button onClick={handelGoToGoogle}>Go to Google</button>
+      <div>
+        <h3>User: {user ? user.displayName : "Not logged in"}</h3>
+      </div>
     </div>
   );
 };
