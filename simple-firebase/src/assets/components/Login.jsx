6c610cc337ad513a2abbf43edc6e9553a4f8687a
mintusikder <mintusikder15@gmail.com>
2024-03-22 17:26:33 +0600

    added github login

--- simple-firebase/src/assets/components/Login.jsx
+++ simple-firebase/src/assets/components/Login.jsx
@@ -1,16 +1,23 @@
 // import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
   const [user, setUser] = useState(null);
-  
+
   const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+
   const handelGoToGoogle = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const loggedInUser = result.user;
         console.log(loggedInUser);
@@ -20,13 +27,46 @@ const Login = () => {
         console.log(error);
       });
   };
-  
+  const handelSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handelGithubLogin =() =>{
+    signInWithPopup(auth,gitHubProvider)
+    .then(result => {
+      const loggedInUser= result.user
+      console.log(loggedInUser)
+      setUser(loggedInUser)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
   return (
     <div>
-      <button onClick={handelGoToGoogle}>Go to Google</button>
-      <div>
-        <h3>User: {user ? user.displayName : "Not logged in"}</h3>
-      </div>
+      {user ? (
+        <button onClick={handelSignOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handelGoToGoogle}>Go to Google</button>
+          <button onClick={handelGithubLogin}>GitHub Login</button>
+        </>
+      )}
+      {user && (
+        <div>
+          <h3>User: {user ? user.displayName : "Not logged in"}</h3>
+          <h3>Email: {user ? user.email : "Not logged in"}</h3>
+          <img src={user ? user.photoURL : "Not Image"} alt="" />
+        </div>
+      )}
     </div>
   );
 };
