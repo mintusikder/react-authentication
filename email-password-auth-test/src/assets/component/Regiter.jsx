19e9497738a46947f49e7e726587dad2cb6d4480
mintusikder <mintusikder15@gmail.com>
2024-03-24 18:03:15 +0600

    added email-password-auth route setup

---
+++ email-password-auth-test/src/assets/component/Regiter.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Regiter = () => {
+    return (
+        <div>
+            <h2>this is rejester</h2>
+        </div>
+    );
+};
+
+export default Regiter;
---
+++ email-password-auth-test/src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './assets/component/Home';
+import Layout from './assets/component/Layout';
+import Login from './assets/component/Login.jsx';
+import Regiter from './assets/component/Regiter';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>,
+      },
+      {
+        path:"/login",
+        element:<Login></Login>,
+      },
+      {
+        path:"/register",
+        element:<Regiter></Regiter>,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
